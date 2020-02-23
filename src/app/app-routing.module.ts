9423182bc811d6fd05ede4287cdd03e416a50ead
shivam dwivedi <shivamdwivedi618@gmail.com>
2020-02-23 15:51:30 +0530

    routing from routing module complted

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { LoginComponent } from './login/login.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '' , component : AboutComponent },
+  { path : 'navigation' ,component: NavigationComponent},
+  { path : 'login' ,component: NavigationComponent},
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
+  declarations: [LoginComponent]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
+// import { Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { MaterialModule } from './material.module';
+import { NavigationComponent } from './navigation/navigation.component';
+import { AboutComponent } from './about/about.component';
+
+
+// const appRoutes:Routes = [
+//   { path: '' , component : AboutComponent },
+
+// ];
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationComponent,
+    AboutComponent
   ],
   imports: [
     BrowserAnimationsModule,

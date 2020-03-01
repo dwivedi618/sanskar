b0c9b4743ef29890f18cae1d00786ad2e5569fec
shivam dwivedi <shivamdwivedi618@gmail.com>
2020-03-01 17:43:05 +0530

    local icons

--- src/app/admission/admission.component.ts
+++ src/app/admission/admission.component.ts
@@ -9,14 +9,16 @@ import { Validators, FormBuilder, FormGroup } from '@angular/forms';
   styleUrls: ['./admission.component.css']
 })
 export class AdmissionComponent implements OnInit {
-  admission = new Admission();
+  
   registerForm: FormGroup;
   submitted = false;
+  imagePreview: any;
 
   constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit() {
       this.registerForm = this.formBuilder.group({
+        image: ['',Validators.required],
           firstName: ['', Validators.required],
           middleName: [''],
           lastName: [''],
@@ -61,6 +63,19 @@ export class AdmissionComponent implements OnInit {
   get ff(){ return this.registerForm.get(['permanentAddress'])['controls']; }//for accessing nested form permanentAddress
   get fff(){ return this.registerForm.get(['localAddress'])['controls']; }//for accessing nested form localAddress
 
+  onImagePicked(event : Event){
+    const file = (event.target as HTMLInputElement).files[0];
+    this.registerForm.patchValue({image : file});
+    this.registerForm.get('image').updateValueAndValidity();
+    // console.log(file);
+    // console.log(this.registerForm);
+    const reader = new FileReader();
+    reader.onload = ()=>{
+      this.imagePreview = reader.result;
+    }
+    reader.readAsDataURL(file);
+
+  }
   onSubmit() {
       this.submitted = true;
 

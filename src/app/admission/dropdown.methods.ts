776184166f4b65647843c211f666d7017879b446
dwivedi618 <shivamdwivedi618@gmail.com>
2022-12-28 01:30:56 +0530

    india dritrict json mapped with state code

--- src/app/admission/dropdown.methods.ts
+++ src/app/admission/dropdown.methods.ts
@@ -1,3 +1,8 @@
+/**
+ * NOTE : these methods must match with  type JsonFormControls.method
+ * IMPORTANT : these method should not match with API_SERVICE_METHOD 
+ * Else in service to get dropdown data recursively call its self
+ */
 export const METHODS = {
     getIndianStates : "indianStates",
     getClasses : "classes",
